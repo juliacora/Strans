5592d0859f1a4984ef3607cbf7b171b7860f84f7
Júlia <juliabcora@gmail.com>
2024-04-19 11:04:50 -0300

    atualizando

--- front/script.js
+++ front/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let button = document.getElementById("botao-enviar");
 
 button.onclick = async function(e) {
@@ -10,6 +11,16 @@ button.onclick = async function(e) {
     let data = {nome, email, telefone, mensagem}
 
     const response = await fetch('http://localhost:3001/api/store/task', {
+=======
+let button = document.getElementById("handleSubmit");
+
+button.onclick = async function() {
+    let tittle = document.getElementById("tittle").value;
+    let description = document.getElementById("description").value;
+    let data = {tittle,description}
+
+    const response = await fetch ('http://localhost:3001/api/store/task', {
+>>>>>>> 382b33769d78835fe1d2d1892a1d921161fccc0c
         method: "POST",
         headers: {"Content-type": "application/json;charset=UTF-8"},
         body: JSON.stringify(data)
@@ -18,8 +29,12 @@ button.onclick = async function(e) {
     let content = await response.json();
 
     if(content.success) {
+<<<<<<< HEAD
         alert("Sucesso");
 
+=======
+        alert("Sucesso")
+>>>>>>> 382b33769d78835fe1d2d1892a1d921161fccc0c
     } else{
         alert('Não');
     }
--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
 const app = require('./app');
 const port = app.get('port');
 
+<<<<<<< HEAD
 app.listen(port, () => console.log(`Rodando a porta ${port}`));
+=======
+app.listen(port, () => console.log(`Rodando a porta ${port}`));
+
+>>>>>>> 382b33769d78835fe1d2d1892a1d921161fccc0c
